51abfbbbba66c65260f7de50132704079d5fbabd
kirano-dev <developer@kirano.uz>
2025-01-03 16:33:19 +0500

    Update seo.php

--- src/routes/seo.php
+++ src/routes/seo.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use KiranoDev\LaravelSeo\Http\Controllers\Api\SeoController;
 
-Route::prefix(config('seo.api_prefix') . '/seo')
+Route::prefix(config('seo.api_prefix') . 'seo')
     ->controller(SeoController::class)
     ->group(function () {
         Route::get('/', 'index');
