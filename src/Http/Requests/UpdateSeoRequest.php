476b5d8cca767fd5ecf2a8b514db12e9a486212f
kirano-dev <developer@kirano.uz>
2025-01-03 16:36:36 +0500

    Update UpdateSeoRequest.php

--- src/Http/Requests/UpdateSeoRequest.php
+++ src/Http/Requests/UpdateSeoRequest.php
@@ -4,13 +4,14 @@ namespace KiranoDev\LaravelSeo\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use KiranoDev\LaravelSeo\Models\Seo;
 
 class UpdateSeoRequest extends FormRequest
 {
     public function rules(): array
     {
         return [
-            'url' => ['required', Rule::unique('seo')->ignore($this->route('seo'), 'url')],
+            'url' => ['required', Rule::unique('seo')->ignore(Seo::findByUrl($this->url), 'url')],
             'title' => ['sometimes'],
             'description' => ['sometimes'],
             'keywords' => ['sometimes'],
