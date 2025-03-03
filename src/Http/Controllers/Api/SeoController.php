ffb9958f8ea09d48e60e75a9de6d3558e2d51132
kirano-dev <developer@kirano.uz>
2025-03-03 14:22:30 +0500

    Update SeoController.php

--- src/Http/Controllers/Api/SeoController.php
+++ src/Http/Controllers/Api/SeoController.php
@@ -5,6 +5,7 @@ namespace KiranoDev\LaravelSeo\Http\Controllers\Api;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
 use KiranoDev\LaravelSeo\Http\Requests\StoreSeoRequest;
 use KiranoDev\LaravelSeo\Http\Requests\UpdateSeoRequest;
@@ -13,7 +14,7 @@ use KiranoDev\LaravelSeo\Models\Seo;
 
 class SeoController extends Controller
 {
-    public function index(Request $request): AnonymousResourceCollection|JsonResponse|SeoResource
+    public function index(Request $request): AnonymousResourceCollection|JsonResponse|SeoResource|Response
     {
         if($request->filled('url')) {
             $seo = Seo::findByUrl($request->url);
@@ -22,9 +23,21 @@ class SeoController extends Controller
                 'data' => Seo::getDefault()
             ]);
 
-            return new SeoResource(
-                cache()->rememberForever($seo->getCacheKey(), fn() => $seo)
-            );
+            $cache = cache()->rememberForever($seo->getCacheKey(), fn() => $seo);
+
+            $eTag = md5(json_encode($cache));
+            $headers = [
+                'ETag' => $eTag,
+                'Cache-Control' => 'public, max-age=0',
+            ];
+
+            if (request()->header('If-None-Match') === $eTag) {
+                return response(status: 304);
+            }
+
+            return response()
+                ->json(new SeoResource($cache))
+                ->withHeaders($headers);
         }
 
         return SeoResource::collection(Seo::all());
